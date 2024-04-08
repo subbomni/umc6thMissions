b0780f5adff312758b67bd6ca5f55b7f63c29ccd
BoMin <suhpmp@gmail.com>
2024-04-08 16:02:10 +0900

    ADD: Week2 Study Mission

---
+++ umcWeek2StudyMission/umcWeek2StudyMission/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  umcWeek2StudyMission
+//
+//  Created by BoMin Lee on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
